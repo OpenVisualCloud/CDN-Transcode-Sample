7319178d106ac26fa9b60ac42efb460ddc6c4c22
Zhou, Cheng <cheng.zhou@intel.com>
2019-04-17 23:35:52 +0800

    Update vlc playback script
    Signed-off-by: Zhou, Cheng <cheng.zhou@intel.com>

--- client/vlc_playback.bat
+++ client/vlc_playback.bat
@@ -16,20 +16,22 @@ rem   |         |         |
 rem   +---------+---------+                                                     
 rem                                                                             
 
+rem Please change the path to the actual VLC executable path in your setup
 C:
 cd "C:\Program Files (x86)\VideoLAN\VLC"
 
-set VM1=192.168.1.107
+rem Please change the IP address to actual CDN-Transcoder IP address in your setup
+set IP_Addr=192.168.1.107
 
-start vlc http://%VM1%:80/hls/big_buck_bunny_2560x1440/index.m3u8  --no-video-deco --no-embedded-video --video-x=1 --video-y=1    --qt-start-minimized --zoom=0.375 -L
+start vlc http://%IP_Addr%:80/hls/big_buck_bunny_2560x1440/index.m3u8  --no-video-deco --no-embedded-video --video-x=1 --video-y=1    --qt-start-minimized --zoom=0.375 -L
 timeout /t 3 /NOBREAK
 
-start vlc http://%VM1%:80/hls/big_buck_bunny_1920x1080/index.m3u8  --no-video-deco --no-embedded-video --video-x=960 --video-y=1    --qt-start-minimized --zoom=0.5 -L
+start vlc http://%IP_Addr%:80/hls/big_buck_bunny_1920x1080/index.m3u8  --no-video-deco --no-embedded-video --video-x=960 --video-y=1    --qt-start-minimized --zoom=0.5 -L
 timeout /t 3 /NOBREAK
 
-start vlc http://%VM1%:80/hls/big_buck_bunny_1280x720/index.m3u8  --no-video-deco --no-embedded-video --video-x=1 --video-y=540    --qt-start-minimized --zoom=0.75 -L
+start vlc http://%IP_Addr%:80/hls/big_buck_bunny_1280x720/index.m3u8  --no-video-deco --no-embedded-video --video-x=1 --video-y=540    --qt-start-minimized --zoom=0.75 -L
 timeout /t 3 /NOBREAK
 
-start vlc http://%VM1%:80/hls/big_buck_bunny_854x480/index.m3u8  --no-video-deco --no-embedded-video --video-x=960 --video-y=540    --qt-start-minimized --zoom=1.12 -L
+start vlc http://%IP_Addr%:80/hls/big_buck_bunny_854x480/index.m3u8  --no-video-deco --no-embedded-video --video-x=960 --video-y=540    --qt-start-minimized --zoom=1.12 -L
 timeout /t 3 /NOBREAK
 
