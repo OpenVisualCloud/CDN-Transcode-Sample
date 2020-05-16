1a723b47706ef8cfde299c3ffd79d753e4dfde45
xwu2git <3871873+xwu2git@users.noreply.github.com>
2020-05-16 08:56:12 -0700

    no wait in kubectl delete

--- deployment/kubernetes/helm/stop.sh
+++ deployment/kubernetes/helm/stop.sh
@@ -6,7 +6,7 @@ helm uninstall cdn-transcode
 
 # delete pvs and scs
 for yaml in $(find "${DIR}" -maxdepth 1 -name "*-pv.yaml" -print); do
-    kubectl delete -f "$yaml" --ignore-not-found=true 2>/dev/null
+    kubectl delete --wait=false -f "$yaml" --ignore-not-found=true 2>/dev/null
 done
 
 kubectl delete secret self-signed-certificate 2> /dev/null || echo -n ""
