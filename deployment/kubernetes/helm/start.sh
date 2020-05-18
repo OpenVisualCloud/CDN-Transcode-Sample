c285d1175b4548f1f9e00f4c1506e9037c24a84b
xwu2git <3871873+xwu2git@users.noreply.github.com>
2020-05-17 18:59:28 -0700

    add registry prefix to the signing image

--- deployment/kubernetes/helm/start.sh
+++ deployment/kubernetes/helm/start.sh
@@ -7,7 +7,8 @@ function create_secret {
 }
 
 # create secrets
-"$DIR/../../certificate/self-sign.sh"
+REGISTRY="$4"
+"$DIR/../../certificate/self-sign.sh" "$REGISTRY"
 create_secret 2>/dev/null || (kubectl delete secret self-signed-certificate; create_secret)
 
 for yaml in $(find "$DIR" -maxdepth 1 -name "*-pv.yaml" -print); do
