c325adc40b52456547e6a08def5430474ce14b55
Pengfei Qu <pengfei.qu@intel.com>
2021-01-29 10:58:29 +0800

    enable cdn on XeonE3 with HW HLS/DASH transcoding

--- common/ffmpegcmd.py
+++ common/ffmpegcmd.py
@@ -81,16 +81,18 @@ class FFMpegCmd:
         self._default_threshold = 4
         self.stream_info(self._in_file)
         self._codec = self._get_codec()
+        # hls and dash
+        self._cmd_static = ["-c:v", self._codec, "-profile:v", "main", "-sc_threshold", "0", "-strict", "-2"]
+        if self._hw == "true":
+            self._cmd_static = ["-profile:v", "main", "-c:v", self._codec]
+        self._cmd_static += ["-g", str(self._keyframe_interval)]
 
 
     def _to_kps(self, bitrate):
         return str(int(bitrate/1000))+"k"
 
     def _get_codec(self):
-        if self._streaming_type == "dash" or self._streaming_type == "hls":
-            return "libx264"
-        else:
-            return codec_setting[self._platform][self._codec_type]
+        return codec_setting[self._platform][self._codec_type]
 
     def stream_info(self, in_file):
         ffprobe_cmd = ["ffprobe", "-v", "quiet", "-print_format", "json", "-show_streams", in_file]
@@ -111,15 +113,10 @@ class FFMpegCmd:
             if segment_duration < self._segment_target_duration:
                 self._segment_target_duration = segment_duration
 
-    def _hls_dash(self):
-        cmd_static = ["-c:v", self._codec, "-profile:v", "main", "-sc_threshold", "0", "-strict", "-2"]
-        cmd_static += ["-g", str(self._keyframe_interval), "-keyint_min", str(self._keyframe_interval)]
-        cmd_dash = ["-use_timeline", "1", "-use_template", "1", "-seg_duration",
-                    str(self._segment_target_duration), "-adaptation_sets", "id=0,streams=v"]
+    def _hls(self):
         cmd_hls = ["-hls_time", str(self._segment_target_duration), "-hls_list_size", "0"]
         cmd_fade_in_out = ["-an"]
         cmd_abr=[]
-
         master_playlist = "#EXTM3U" + "\n" + "#EXT-X-VERSION:3" +"\n" + "#" + "\n"
         count = 0
         for item in self._renditions:
@@ -128,44 +125,51 @@ class FFMpegCmd:
             v_bitrate = self._to_kps(item[2])
             a_bitrate = self._to_kps(item[3])
             maxrate = self._to_kps(item[2] * self._max_bitrate_ratio)
-            bufsize = self._to_kps(item[2] * self._rate_monitor_buffer_ratio)
             name = str(height) + "p"
             if self._frame_height < height:
                 continue
-            cmd_1 = []
-            cmd_2 = []
-            cmd_3 = []
-            cmd_4 = []
-            if self._streaming_type == "hls":
-                cmd_1 = ["-vf", "scale=w="+str(width)+":"+"h="+str(height)]
-                cmd_2 = ["-b:v", v_bitrate, "-maxrate", maxrate, "-bufsize", bufsize]
-                cmd_3 = ["-f", self._streaming_type]
-                cmd_4 = ["-hls_segment_filename", self._target+"/"+name+"_"+"%03d.ts", self._target+"/"+name+".m3u8"]
-                master_playlist += "#EXT-X-STREAM-INF:BANDWIDTH="+str(item[2])+","+"RESOLUTION="+str(width)+"x"+str(height)+"\n"+name+".m3u8"+"\n"
-                cmd_abr += cmd_static + cmd_1 + cmd_2 + cmd_fade_in_out + cmd_3 + cmd_hls + cmd_4
-            elif self._streaming_type == "dash":
-                cmd_1 = ["-map", "0:v", "-b:v"+":"+str(count), v_bitrate, "-s:v"+":"+str(count), str(width)+"x"+str(height),
-                         "-maxrate"+":"+str(count), maxrate, "-bufsize"+":"+str(count), bufsize]
-                cmd_2 = ["-an"]
-                cmd_3 = ["-f", self._streaming_type]
-                cmd_4 = ["-init_seg_name", name+"-init-stream$RepresentationID$.m4s", "-media_seg_name",
-                         name+"-chunk-stream$RepresentationID$-$Number%05d$.m4s", "-y", self._target+"/"+name+".mpd"]
-                if self._clip_a_duration == 0:
-                    cmd_1 = ["-map", "0:v", "-b:v"+":"+str(count), v_bitrate, "-s:v"+":"+str(count), str(width)+"x"+str(height),
-                             "-maxrate"+":"+str(count), maxrate, "-bufsize"+":"+str(count), bufsize]
-                    cmd_2 = []
-                cmd_abr += cmd_1 + cmd_2
+
+            cmd_1 = ["-vf", "scale=w="+str(width)+":"+"h="+str(height)]
+            if self._hw == "true":
+                cmd_1 = ["-vf", "scale_vaapi=w="+str(width)+":"+"h="+str(height)+":format=nv12"]
+            cmd_2 = ["-b:v", v_bitrate, "-maxrate", maxrate]
+            cmd_3 = ["-f", self._streaming_type]
+            cmd_4 = ["-hls_segment_filename", self._target+"/"+name+"_"+"%03d.ts", self._target+"/"+name+".m3u8"]
+            master_playlist += "#EXT-X-STREAM-INF:BANDWIDTH="+str(item[2])+","+"RESOLUTION="+str(width)+"x"+str(height)+"\n"+name+".m3u8"+"\n"
+            cmd_abr += cmd_1 + self._cmd_static + cmd_1 + cmd_2 + cmd_fade_in_out + cmd_3 + cmd_hls + cmd_4
 
             count += 1
             if count > self._default_threshold:
                 break
+        with open(self._target+"/"+"index.m3u8", "w", encoding='utf-8') as f:
+            f.write(master_playlist)
+        return cmd_abr
 
-        if self._streaming_type == "hls":
-            with open(self._target+"/"+"index.m3u8", "w", encoding='utf-8') as f:
-                f.write(master_playlist)
-            return cmd_abr
-        elif self._streaming_type == "dash":
-            return cmd_static + cmd_abr +["-f", "dash"] + cmd_dash + ["-y", self._target+"/"+"index.mpd"]
+    def _dash(self):
+        cmd_dash = ["-use_timeline", "1", "-use_template", "1", "-seg_duration", str(self._segment_target_duration), "-adaptation_sets", "id=0,streams=v"]
+        cmd_abr=[]
+        cmd_scale=[]
+
+        count = 0
+        for item in self._renditions:
+            width = item[0]
+            height = item[1]
+            v_bitrate = self._to_kps(item[2])
+            a_bitrate = self._to_kps(item[3])
+            maxrate = self._to_kps(item[2] * self._max_bitrate_ratio)
+            if self._frame_height < height:
+                continue
+            cmd_1 = ["-map", "[out"+str(count) +"]", "-b:v"+":"+str(count), v_bitrate, "-maxrate"+":"+str(count), maxrate]
+            if self._hw == "true":
+                cmd_scale += [";", "[mid"+str(count) +"]", "scale_vaapi=w="+str(width)+":"+"h="+str(height)+":format=nv12","[out"+str(count) +"]"]
+            else:
+                cmd_scale += [";", "[mid"+str(count) +"]", "scale=w="+str(width)+":"+"h="+str(height),"[out"+str(count) +"]"]
+            cmd_abr += cmd_1
+            count += 1
+            if count > self._default_threshold:
+                break
+        cmd_scale = ["[0:v]split="+str(count)]+["[mid"+str(_id) +"]" for _id in range(count)]+cmd_scale
+        return ["-filter_complex"] +["".join(cmd_scale)]+ self._cmd_static + cmd_abr +["-f", "dash"] + cmd_dash + ["-y", self._target+"/"+"index.mpd"]
 
     def _tc(self):
         cmd_1 = []
@@ -176,12 +180,11 @@ class FFMpegCmd:
             v_bitrate = self._to_kps(item[2])
             a_bitrate = self._to_kps(item[3])
             maxrate = self._to_kps(item[2] * self._max_bitrate_ratio)
-            bufsize = self._to_kps(item[2] * self._rate_monitor_buffer_ratio)
             name= self._target+"/"+self._codec_type+"_"+str(height)+"p."+self._streaming_type if self._streaming_type == "mp4" else self._target+"_"+self._codec_type+str(height)+"p"
 
             if self._hw == "true":
                 cmd_1 += ["-vf", "scale_vaapi=w="+str(width)+":"+"h="+str(height)+":format=nv12", "-c:v", self._codec]
-                cmd_1 += ["-profile", "578", "-level", "30", "-b:v", v_bitrate, "-maxrate", v_bitrate, "-r", params["framerate"],"-g", params["gop_size"], "-bf", params["bframe"], "-an", "-f", self._streaming_type, name]
+                cmd_1 += ["-profile:v", "main", "-b:v", v_bitrate, "-maxrate", v_bitrate, "-r", params["framerate"],"-g", params["gop_size"], "-bf", params["bframe"], "-an", "-f", self._streaming_type, name]
             else:
                 cmd_1 += ["-vf", "scale=w="+str(width)+":"+"h="+str(height),"-c:v", self._codec, "-b:v", v_bitrate]
                 cmd_1 += ["-r", params["framerate"],"-g", params["gop_size"], "-bf", params["bframe"], "-refs", params["refs"], "-preset", params["preset"], "-forced-idr", params["forced_idr"], "-an", "-f", self._streaming_type, name]
@@ -190,8 +193,10 @@ class FFMpegCmd:
 
     def cmd(self):
         cmd = []
-        if self._streaming_type == "hls" or self._streaming_type == "dash":
-            cmd = self._cmd_base + self._hls_dash()
+        if self._streaming_type == "hls":
+            cmd = self._cmd_base + self._hls()
+        if self._streaming_type == "dash":
+            cmd = self._cmd_base + self._dash()
         elif self._streaming_type == "mp4":
             cmd = self._cmd_base + self._tc()
         return cmd
